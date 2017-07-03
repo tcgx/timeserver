077bb8c816a9b482699d9a789b0c017ade0379e6
tcgx <xuangao@outlook.com>
2017-07-03 22:25:42 +0800

    Go 返回时间戳的服务
    只是简单的返回json时间戳，测试qps能达到多少

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/labstack/gommon/log"
+	"net/http"
+	"time"
+)
+
+var resJson *res
+
+func init() {
+	resJson = &res{}
+}
+
+type (
+	res struct {
+		Timestamp int64 `json:"timestamp"`
+	}
+)
+
+func getTime(w http.ResponseWriter, req *http.Request) {
+
+	resJson.Timestamp = time.Now().Unix()
+	fmt.Println(resJson)
+	resByte, err := json.Marshal(resJson)
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Header().Set("content-type", "application/json;charset=utf-8")
+	w.Write(resByte)
+}
+
+func main() {
+	http.HandleFunc("/", getTime)
+	err := http.ListenAndServe(":1323", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
